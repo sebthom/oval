c9673a5c6dba92f107bc44c1dbc0c870e2d16c96
sebthom <sebthom@cbba0f0e-2014-0410-be5a-d2c2f4c84efa>
2006-08-04 17:57:25 +0000

    v0.6 changes

---
+++ net.sf.oval/src/net/sf/oval/constraints/AssertValidCheck.java
@@ -0,0 +1,34 @@
+/*******************************************************************************
+ * Portions created by Sebastian Thomschke are copyright (c) 2005, 2006 Sebastian
+ * Thomschke.
+ * 
+ * All Rights Reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Sebastian Thomschke - initial implementation.
+ *******************************************************************************/
+package net.sf.oval.constraints;
+
+import net.sf.oval.AbstractCheck;
+import net.sf.oval.contexts.OValContext;
+
+/**
+ * @author Sebastian Thomschke
+ * @version $Revision: 1.0 $
+ */
+public class AssertValidCheck extends AbstractCheck<AssertValid>
+{
+	/**
+	 *  This method is not used.
+	 *  The validation of this special constraint is directly performed by the Validator class
+	 */
+	public boolean isSatisfied(final Object validatedObject, final Object value,
+			final OValContext context)
+	{
+		// ignored
+		return true;
+	}
+}
