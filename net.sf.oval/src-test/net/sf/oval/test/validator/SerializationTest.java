0f171db8fb2d08002a17b9c73bcfdc605a0fa2a8
sebthom <sebthom@cbba0f0e-2014-0410-be5a-d2c2f4c84efa>
2006-11-02 11:07:36 +0000

    v0.7 changes

---
+++ net.sf.oval/src-test/net/sf/oval/test/validator/SerializationTest.java
@@ -0,0 +1,64 @@
+/*******************************************************************************
+ * Portions created by Sebastian Thomschke are copyright (c) 2005, 2006 Sebastian
+ * Thomschke.
+ * 
+ * All Rights Reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Sebastian Thomschke - initial implementation.
+ *******************************************************************************/
+package net.sf.oval.test.validator;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+import java.util.List;
+
+import junit.framework.TestCase;
+import net.sf.oval.ConstraintViolation;
+import net.sf.oval.Validator;
+import net.sf.oval.constraints.Length;
+
+/**
+ * @author Sebastian Thomschke
+ * @version $Revision: 1.1 $
+ */
+public class SerializationTest extends TestCase
+{
+	private static class Person implements Serializable
+	{
+		private static final long serialVersionUID = 1L;
+
+		@Length(max = 5)
+		public String firstName;
+	}
+
+	@SuppressWarnings("unchecked")
+	public void testSerialization() throws IOException, ClassNotFoundException
+	{
+		final Validator validator = new Validator();
+
+		final Person p = new Person();
+		p.firstName = "123456";
+		List<ConstraintViolation> violations = validator.validate(p);
+		assertTrue(violations.size() == 1);
+
+		// serialize the violations
+		ByteArrayOutputStream bos = new ByteArrayOutputStream();
+		ObjectOutputStream oos = new ObjectOutputStream(bos);
+		oos.writeObject(violations);
+		oos.flush();
+		byte[] bytes = bos.toByteArray();
+
+		// deserialize the violations
+		ByteArrayInputStream bis = new ByteArrayInputStream(bytes);
+		ObjectInputStream ois = new ObjectInputStream(bis);
+		violations = (List<ConstraintViolation>) ois.readObject();
+	}
+}
