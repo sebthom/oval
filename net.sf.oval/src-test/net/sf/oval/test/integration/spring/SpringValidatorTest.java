a6ac6790fe98a43a473fb9ae661b882d01e5d308
sebthom <sebthom@cbba0f0e-2014-0410-be5a-d2c2f4c84efa>
2008-01-03 23:04:48 +0000

    v1.20 changes - documentation updates

--- net.sf.oval/src-test/net/sf/oval/test/integration/spring/SpringValidatorTest.java
+++ net.sf.oval/src-test/net/sf/oval/test/integration/spring/SpringValidatorTest.java
@@ -1,6 +1,15 @@
-/**
+/*******************************************************************************
+ * Portions created by Sebastian Thomschke are copyright (c) 2005-2007 Sebastian
+ * Thomschke.
  * 
- */
+ * All Rights Reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Sebastian Thomschke - initial implementation.
+ *******************************************************************************/
 package net.sf.oval.test.integration.spring;
 
 import junit.framework.TestCase;
@@ -12,8 +21,7 @@ import net.sf.oval.integration.spring.SpringValidator;
 import org.springframework.validation.BindException;
 
 /**
- * @author Administrator
- *
+ * @author Sebastian Thomschke
  */
 public class SpringValidatorTest extends TestCase
 {
