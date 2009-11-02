660c79f5b71e07d2e72fb8daa8c8e30da4b9818c
sebthom <sebthom@cbba0f0e-2014-0410-be5a-d2c2f4c84efa>
2009-11-02 19:03:58 +0000

    fixed unit testing, suppressed logging of irrelevant MethodNotFoundException

--- trunk/src/main/java/net/sf/oval/configuration/annotation/AbstractAnnotationCheck.java
+++ trunk/src/main/java/net/sf/oval/configuration/annotation/AbstractAnnotationCheck.java
@@ -18,6 +18,7 @@ import java.lang.reflect.Method;
 import net.sf.oval.AbstractCheck;
 import net.sf.oval.ConstraintTarget;
 import net.sf.oval.internal.Log;
+import net.sf.oval.internal.util.ReflectionUtils;
 
 /**
  * Partial implementation of check classes configurable via annotations.
@@ -44,97 +45,117 @@ public abstract class AbstractAnnotationCheck<ConstraintAnnotation extends Annot
 		 * Using reflection is required because annotations do not support inheritance and 
 		 * therefore cannot implement an interface that could be used for a down cast here.
 		 */
-		try
-		{
-			final Method getMessage = constraintClazz.getDeclaredMethod("message", (Class< ? >[]) null);
-			setMessage((String) getMessage.invoke(constraintAnnotation, (Object[]) null));
-		}
-		catch (final Exception e)
-		{
-			LOG
-					.debug("Cannot determine constraint error message based on annotation {1}", constraintClazz
-							.getName(), e);
+		final Method getMessage = ReflectionUtils.getMethod(constraintClazz, "message", (Class< ? >[]) null);
+		if (getMessage == null)
+			LOG.debug("Cannot determine constraint error message based on annotation {1} since attribtue message() is not defined.", constraintClazz.getName());
+		else
 			try
 			{
-				setMessage(constraintClazz.getName() + ".violated");
+				setMessage((String) getMessage.invoke(constraintAnnotation, (Object[]) null));
 			}
-			catch (final UnsupportedOperationException ex)
+			catch (final Exception ex)
 			{
-				// ignore
+				LOG.warn("Cannot determine constraint error message based on annotation {1}",
+						constraintClazz.getName(), ex);
+
+				try
+				{
+					setMessage(constraintClazz.getName() + ".violated");
+				}
+				catch (final UnsupportedOperationException uex)
+				{
+					// ignore
+				}
 			}
-		}
 
 		/*
 		 * Retrieve the appliesTo value from the constraint annotation via reflection.
 		 */
-		try
-		{
-			final Method getAppliesTo = constraintClazz.getDeclaredMethod("appliesTo", (Class< ? >[]) null);
-			setAppliesTo((ConstraintTarget[]) getAppliesTo.invoke(constraintAnnotation, (Object[]) null));
-		}
-		catch (final Exception e)
-		{
-			LOG.debug("Cannot determine constraint targets based on annotation {1}", constraintClazz.getName(), e);
-		}
-		
+		final Method getAppliesTo = ReflectionUtils.getMethod(constraintClazz, "appliesTo", (Class< ? >[]) null);
+		if (getAppliesTo == null)
+			LOG.debug("Cannot determine constraint targets based on annotation {1} since attribtue appliesTo() is not defined.", constraintClazz.getName());
+		else
+			try
+			{
+				setAppliesTo((ConstraintTarget[]) getAppliesTo.invoke(constraintAnnotation, (Object[]) null));
+			}
+			catch (final Exception ex)
+			{
+				LOG.warn("Cannot determine constraint targets based on annotation {1}", constraintClazz.getName(), ex);
+			}
+
 		/*
 		 * Retrieve the error code value from the constraint annotation via reflection.
 		 */
-		try
-		{
-			final Method getErrorCode = constraintClazz.getDeclaredMethod("errorCode", (Class< ? >[]) null);
-			setErrorCode((String) getErrorCode.invoke(constraintAnnotation, (Object[]) null));
-		}
-		catch (final Exception e)
-		{
-			LOG.debug("Cannot determine constraint error code based on annotation {1}", constraintClazz.getName(), e);
+		final Method getErrorCode = ReflectionUtils.getMethod(constraintClazz, "errorCode", (Class< ? >[]) null);
+		if (getErrorCode == null)
+			LOG.debug("Cannot determine constraint error code based on annotation {1} since attribtue errorCode() is not defined.", constraintClazz.getName());
+		else
 			try
 			{
-				setErrorCode(constraintClazz.getName());
+				setErrorCode((String) getErrorCode.invoke(constraintAnnotation, (Object[]) null));
 			}
-			catch (final UnsupportedOperationException ex)
+			catch (final Exception ex)
 			{
-				// ignore
+				LOG.warn("Cannot determine constraint error code based on annotation {1}", constraintClazz.getName(),
+						ex);
+				try
+				{
+					setErrorCode(constraintClazz.getName());
+				}
+				catch (final UnsupportedOperationException uex)
+				{
+					// ignore
+				}
 			}
-		}
 
 		/*
 		 * Retrieve the severity value from the constraint annotation via reflection.
 		 */
-		try
-		{
-			final Method getSeverity = constraintClazz.getDeclaredMethod("severity", (Class< ? >[]) null);
-			setSeverity(((Number) getSeverity.invoke(constraintAnnotation, (Object[]) null)).intValue());
-		}
-		catch (final Exception e)
-		{
-			LOG.debug("Cannot determine constraint severity based on annotation {1}", constraintClazz.getName(), e);
-		}
+		final Method getSeverity = ReflectionUtils.getMethod(constraintClazz, "severity", (Class< ? >[]) null);
+		if (getSeverity == null)
+			LOG.debug("Cannot determine constraint severity based on annotation {1} since attribtue severity() is not defined.", constraintClazz.getName());
+		else
+			try
+			{
+				setSeverity(((Number) getSeverity.invoke(constraintAnnotation, (Object[]) null)).intValue());
+			}
+			catch (final Exception ex)
+			{
+				LOG.warn("Cannot determine constraint severity based on annotation {1}", constraintClazz.getName(), ex);
+			}
 
 		/*
 		 * Retrieve the profiles value from the constraint annotation via reflection.
 		 */
-		try
-		{
-			final Method getProfiles = constraintClazz.getDeclaredMethod("profiles", (Class< ? >[]) null);
-			setProfiles((String[]) getProfiles.invoke(constraintAnnotation, (Object[]) null));
-		}
-		catch (final Exception e)
-		{
-			LOG.debug("Cannot determine constraint profiles based on annotation {1}", constraintClazz.getName(), e);
-		}
+		final Method getProfiles = ReflectionUtils.getMethod(constraintClazz, "profiles", (Class< ? >[]) null);
+		if (getProfiles == null)
+			LOG.debug("Cannot determine constraint profiles based on annotation {1} since attribtue profiles() is not defined.", constraintClazz.getName());
+		else
+			try
+			{
+				setProfiles((String[]) getProfiles.invoke(constraintAnnotation, (Object[]) null));
+			}
+			catch (final Exception ex)
+			{
+				LOG.warn("Cannot determine constraint profiles based on annotation {1}", constraintClazz.getName(), ex);
+			}
 
 		/*
 		 * Retrieve the when formula from the constraint annotation via reflection.
 		 */
-		try
-		{
-			final Method getWhen = constraintClazz.getDeclaredMethod("when", (Class< ? >[]) null);
-			setWhen((String) getWhen.invoke(constraintAnnotation, (Object[]) null));
-		}
-		catch (final Exception e)
-		{
-			LOG.debug("Cannot determine constraint when formula based on annotation {1}", constraintClazz.getName(), e);
-		}
+		final Method getWhen = ReflectionUtils.getMethod(constraintClazz, "when", (Class< ? >[]) null);
+		if (getWhen == null)
+			LOG.debug("Cannot determine constraint when formula based on annotation {1} since attribtue when() is not defined.", constraintClazz.getName());
+		else
+			try
+			{
+				setWhen((String) getWhen.invoke(constraintAnnotation, (Object[]) null));
+			}
+			catch (final Exception ex)
+			{
+				LOG.warn("Cannot determine constraint when formula based on annotation {1}", constraintClazz.getName(),
+						ex);
+			}
 	}
 }
