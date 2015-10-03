1c3999a5b8cb9676003039cae6f9b300f768f16b
sebthom <sebthom@users.noreply.github.com>
2015-10-03 21:33:29 +0200

    update

--- mvn-release.cmd
+++ mvn-release.cmd
@@ -1,4 +1,4 @@
 @echo off
 set /p passphrase=Enter GPG Password:
 
-call mvn -e release:prepare -Darguments="-Dgpg.passphrase=%passphrase%" && call mvn -e release:perform -Darguments="-Dgpg.passphrase=%passphrase%"
+call %MVN% -e release:prepare -Darguments="-Dgpg.passphrase=%passphrase%" && call %MVN% -e release:perform -Darguments="-Dgpg.passphrase=%passphrase%"
