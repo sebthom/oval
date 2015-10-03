65bddf810bd205aef6cd0453583bc3bf2c4ef7f8
sebthom <sebthom@users.noreply.github.com>
2015-10-03 20:33:31 +0200

    update

--- mvn-release-no-tests.cmd
+++ mvn-release-no-tests.cmd
@@ -1,4 +1,4 @@
 @echo off
 set /p passphrase=Enter GPG Password:
 
-mvn -e release:prepare -Darguments="-DskipTests -Dgpg.passphrase=%passphrase%" && mvn -e release:perform -Darguments="-DskipTests -Dgpg.passphrase=%passphrase%"
+%MVN% -e release:prepare -Darguments="-DskipTests -Dgpg.passphrase=%passphrase%" && mvn -e release:perform -Darguments="-DskipTests -Dgpg.passphrase=%passphrase%"
--- mvn-release.cmd
+++ mvn-release.cmd
@@ -1,4 +1,4 @@
 @echo off
 set /p passphrase=Enter GPG Password:
 
-mvn -e release:prepare -Darguments="-Dgpg.passphrase=%passphrase%" && mvn -e release:perform -Darguments="-Dgpg.passphrase=%passphrase%"
+%MVN% -e release:prepare -Darguments="-Dgpg.passphrase=%passphrase%" && mvn -e release:perform -Darguments="-Dgpg.passphrase=%passphrase%"
