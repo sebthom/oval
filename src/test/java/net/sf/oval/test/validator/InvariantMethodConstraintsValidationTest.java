d79a5193b21ba4c85c7c34fa88853dae481d62ef
Sebastian Thomschke <sebthom@users.noreply.github.com>
2018-05-07 21:16:21 +0200

    @SuppressWarnings("unused")

--- src/test/java/net/sf/oval/test/validator/InvariantMethodConstraintsValidationTest.java
+++ src/test/java/net/sf/oval/test/validator/InvariantMethodConstraintsValidationTest.java
@@ -1,12 +1,12 @@
 /*******************************************************************************
  * Portions created by Sebastian Thomschke are copyright (c) 2005-2017 Sebastian
  * Thomschke.
- * 
+ *
  * All Rights Reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- * 
+ *
  * Contributors:
  *     Sebastian Thomschke - initial implementation.
  *******************************************************************************/
@@ -37,6 +37,7 @@ public class InvariantMethodConstraintsValidationTest extends TestCase {
         }
     }
 
+    @SuppressWarnings("unused")
     public static class TestEntityInvalidConfig extends TestEntity {
         /**
          * the @NotNull annotation should lead to a "OVal API usage violation 7" warning by the ApiUsageAuditor
