a9e59f2db084ae92fabbb780eaaf718f146bb55a
Sebastian Thomschke <sebthom@users.noreply.github.com>
2018-05-08 09:50:56 +0200

    Run AssertURL tests as integration tests

--- src/test/java/net/sf/oval/test/constraints/AssertURLITest.java
+++ src/test/java/net/sf/oval/test/constraints/AssertURLITest.java
@@ -16,7 +16,7 @@ import net.sf.oval.constraint.AssertURLCheck.URIScheme;
  * @author Makkari - initial implementation
  * @author Sebastian Thomschke
  */
-public class AssertURLTest extends AbstractContraintsTest {
+public class AssertURLITest extends AbstractContraintsTest {
     private static final URIScheme[] PERMITTED_SCHEMES = { URIScheme.FTP, URIScheme.HTTP, URIScheme.HTTPS };
 
     public void testAssertURL() {
@@ -24,13 +24,13 @@ public class AssertURLTest extends AbstractContraintsTest {
         super.testCheck(check);
         assertNull(check.getPermittedURISchemes());
 
-        check.setPermittedURISchemes(AssertURLTest.PERMITTED_SCHEMES);
+        check.setPermittedURISchemes(PERMITTED_SCHEMES);
         final URIScheme[] actualPermittedSchemes = check.getPermittedURISchemes();
         assertNotNull(actualPermittedSchemes);
-        assertEquals(AssertURLTest.PERMITTED_SCHEMES.length, actualPermittedSchemes.length);
-        for (int n = 0; n < AssertURLTest.PERMITTED_SCHEMES.length; n++) {
-            assertEquals(AssertURLTest.PERMITTED_SCHEMES[n], actualPermittedSchemes[n]);
-            assertEquals(AssertURLTest.PERMITTED_SCHEMES[n].toString(), actualPermittedSchemes[n].getScheme());
+        assertEquals(PERMITTED_SCHEMES.length, actualPermittedSchemes.length);
+        for (int n = 0; n < PERMITTED_SCHEMES.length; n++) {
+            assertEquals(PERMITTED_SCHEMES[n], actualPermittedSchemes[n]);
+            assertEquals(PERMITTED_SCHEMES[n].toString(), actualPermittedSchemes[n].getScheme());
         }
 
         assertFalse(check.isConnect());
@@ -45,7 +45,7 @@ public class AssertURLTest extends AbstractContraintsTest {
         assertTrue(check.isSatisfied(this, "http://www.google.com", null, validator));
         assertTrue(check.isSatisfied(this, "https://www.google.com", null, validator));
         assertTrue(check.isSatisfied(this, "httPs://www.google.com", null, validator));
-        assertTrue(check.isSatisfied(this, "ftp://ftp.is.co.za/rfc/rfc1808.txt", null, validator));
+        assertTrue(check.isSatisfied(this, "ftp://ftp.uni-erlangen.de/debian/README.mirrors.txt", null, validator));
         assertFalse(check.isSatisfied(this, "ptth://www.google.com", null, validator));
         assertFalse(check.isSatisfied(this, "http://www.g[oogle.com", null, validator));
 
@@ -55,10 +55,10 @@ public class AssertURLTest extends AbstractContraintsTest {
         assertFalse(check.isSatisfied(this, "https", null, validator));
         assertFalse(check.isSatisfied(this, "ftp", null, validator));
         assertTrue(check.isSatisfied(this, "http://www.google.com", null, validator));
-        //assertTrue(check.isSatisfied(this, "https://www.verisign.com/site-map/index.html", null, validator));
+        assertTrue(check.isSatisfied(this, "https://www.google.com", null, validator));
         assertFalse(check.isSatisfied(this, "http://127.0.0.1:34343", null, validator));
-        assertTrue(check.isSatisfied(this, "ftp://speedtest.tele2.net/512KB.zip", null, validator));
-        assertFalse(check.isSatisfied(this, "ftp://ftp.debian.org/debian/foo.html", null, validator));
+        assertFalse(check.isSatisfied(this, "ftp://ftp.uni-erlangen.de/debian/foo.html", null, validator));
+        assertTrue(check.isSatisfied(this, "ftp://ftp.uni-erlangen.de/debian/README.mirrors.txt", null, validator));
 
         check.setPermittedURISchemes(null);
     }
