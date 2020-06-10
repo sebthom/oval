69330c20c991fec8872e1527e93d1c81a0ff16f0
sebthom <sebthom@users.noreply.github.com>
2020-06-10 21:10:47 +0200

    update build config

--- .ci/build.sh
+++ .ci/build.sh
@@ -10,68 +10,128 @@ set -e # abort script at first error
 set -o pipefail # causes a pipeline to return the exit status of the last command in the pipe that returned a non-zero return value
 
 if [[ -f .ci/release-trigger.sh ]]; then
-    echo "Sourcing [.ci/release-trigger.sh]..."
-    source .ci/release-trigger.sh
+   echo "Sourcing [.ci/release-trigger.sh]..."
+   source .ci/release-trigger.sh
 fi
+
+
+echo
+echo "###################################################"
+echo "# Determining GIT branch......                    #"
+echo "###################################################"
+if [[ $CI == "true" && $TRAVIS == "true" && $USER == "travis" ]]; then
+   # https://docs.travis-ci.com/user/environment-variables/#default-environment-variables
+   if [[ $TRAVIS_PULL_REQUEST == "false" ]]; then
+      GIT_BRANCH="$TRAVIS_BRANCH"
+   else
+      GIT_BRANCH="$TRAVIS_PULL_REQUEST_BRANCH"
+   fi
+else
+   GIT_BRANCH=$(git branch --show-current)
+fi
+echo "  -> GIT Branch: $GIT_BRANCH"; echo
+
 
 MAVEN_VERSION=3.6.3 # http://maven.apache.org/download.cgi
 MAVEN_HELP_PLUGIN_VERSION=3.2.0 # https://search.maven.org/artifact/org.apache.maven.plugins/maven-help-plugin
 if [[ ! -e $HOME/.m2/bin/apache-maven-$MAVEN_VERSION ]]; then
-    echo "Installing Maven version $MAVEN_VERSION..."
-    mkdir -p $HOME/.m2/bin/
-    wget --quiet https://archive.apache.org/dist/maven/maven-3/$MAVEN_VERSION/binaries/apache-maven-$MAVEN_VERSION-bin.tar.gz -O /tmp/maven.tar.gz
-    tar xfz /tmp/maven.tar.gz -C $HOME/.m2/bin/
+   echo
+   echo "###################################################"
+   echo "# Installing Maven version $MAVEN_VERSION...               #"
+   echo "###################################################"
+   mkdir -p $HOME/.m2/bin/
+   wget --quiet https://archive.apache.org/dist/maven/maven-3/$MAVEN_VERSION/binaries/apache-maven-$MAVEN_VERSION-bin.tar.gz -O /tmp/maven.tar.gz
+   tar xfz /tmp/maven.tar.gz -C $HOME/.m2/bin/
 fi
 export M2_HOME=$HOME/.m2/bin/apache-maven-$MAVEN_VERSION
 export PATH=$M2_HOME/bin:$PATH
 
+
+echo
+echo "###################################################"
+echo "# Configuring JDK Class Data Sharing...           #"
+echo "###################################################"
+# https://docs.oracle.com/javase/8/docs/technotes/guides/vm/class-data-sharing.html
+jdk_version_checksum=$(java -version 2>&1 | md5sum | cut -f1 -d" ")
+if [[ ! -f $HOME/.xshare/$jdk_version_checksum ]]; then
+   echo "  -> Generating shared class data archive..."
+   mkdir -p $HOME/.xshare
+   java -Xshare:dump -XX:+UnlockDiagnosticVMOptions -XX:SharedArchiveFile=$HOME/.xshare/$jdk_version_checksum
+else
+   echo "  -> Reusing shared class data archive..."
+fi
+export JAVA_TOOL_OPTIONS="${JAVA_TOOL_OPTIONS:-} -Xshare:on -XX:+UnlockDiagnosticVMOptions -XX:SharedArchiveFile=$HOME/.xshare/$jdk_version_checksum"
+
+
+echo
+echo "###################################################"
+echo "# Configuring MAVEN_OPTS...                       #"
+echo "###################################################"
+MAVEN_OPTS="${MAVEN_OPTS:-}"
+MAVEN_OPTS="$MAVEN_OPTS -XX:+TieredCompilation -XX:TieredStopAtLevel=1" # https://zeroturnaround.com/rebellabs/your-maven-build-is-slow-speed-it-up/
+MAVEN_OPTS="$MAVEN_OPTS -Djava.security.egd=file:/dev/./urandom" # https://stackoverflow.com/questions/58991966/what-java-security-egd-option-is-for/59097932#59097932
+MAVEN_OPTS="$MAVEN_OPTS -Dorg.slf4j.simpleLogger.showDateTime=true -Dorg.slf4j.simpleLogger.dateTimeFormat=HH:mm:ss,SSS" # https://stackoverflow.com/questions/5120470/how-to-time-the-different-stages-of-maven-execution/49494561#49494561
+export MAVEN_OPTS="$MAVEN_OPTS -Xmx1024m -Djava.awt.headless=true -Djava.net.preferIPv4Stack=true -Dhttps.protocols=TLSv1.2"
+echo "  -> MAVEN_OPTS: $MAVEN_OPTS"
+
+
+echo
+echo "###################################################"
+echo "# Determining current Maven project version...    #"
+echo "###################################################"
 # https://stackoverflow.com/questions/3545292/how-to-get-maven-project-version-to-the-bash-command-line
-echo "Determining current Maven project version..."
 projectVersion="$(mvn -s .ci/maven_settings.xml org.apache.maven.plugins:maven-help-plugin:$MAVEN_HELP_PLUGIN_VERSION:evaluate -Dexpression=project.version -q -DforceStdout)"
 echo "  -> Current Version: $projectVersion"
 
-MAVEN_OPTS="-XX:+TieredCompilation -XX:TieredStopAtLevel=1" # https://zeroturnaround.com/rebellabs/your-maven-build-is-slow-speed-it-up/
-export MAVEN_OPTS="${MAVEN_OPTS} -Xmx1024m -Djava.awt.headless=true -Djava.net.preferIPv4Stack=true"
+
+MAVEN_CLI_OPTS="-e -U --batch-mode --show-version --no-transfer-progress -s .ci/maven_settings.xml -t .ci/maven_toolchains.xml"
+
+sed -i -E "s/(<java.version>).*(<\/java.version>)/\1${JAVA_VERSION}\2/" pom.xml
 
 #
-# decide whether to build/deploy a snapshot version or perform a release build
+# decide whether to perform a release build or build+deploy a snapshot version
 #
-if [[ ${projectVersion:-foo} == ${POM_CURRENT_VERSION:-bar} ]]; then
-    # https://stackoverflow.com/questions/8653126/how-to-increment-version-number-in-a-shell-script/21493080#21493080
-    nextDevelopmentVersion="$(echo ${POM_RELEASE_VERSION} | perl -pe 's/^((\d+\.)*)(\d+)(.*)$/$1.($3+1).$4/e')-SNAPSHOT"
+if [[ ${projectVersion:-foo} == ${POM_CURRENT_VERSION:-bar} && ${MAY_CREATE_RELEASE:-false} == "true" ]]; then
+   # https://stackoverflow.com/questions/8653126/how-to-increment-version-number-in-a-shell-script/21493080#21493080
+   nextDevelopmentVersion="$(echo ${POM_RELEASE_VERSION} | perl -pe 's/^((\d+\.)*)(\d+)(.*)$/$1.($3+1).$4/e')-SNAPSHOT"
 
-    echo "###################################################"
-    echo "# Creating Maven Release...                       #"
-    echo "###################################################"
-    echo "  ->          Release Version: ${POM_RELEASE_VERSION}"
-    echo "  -> Next Development Version: ${nextDevelopmentVersion}"
-    echo "  ->           Skipping Tests: ${SKIP_TESTS}"
-    echo "  ->               Is Dry-Run: ${DRY_RUN}"
+   echo
+   echo "###################################################"
+   echo "# Creating Maven Release...                       #"
+   echo "###################################################"
+   echo "  ->          Release Version: ${POM_RELEASE_VERSION}"
+   echo "  -> Next Development Version: ${nextDevelopmentVersion}"
+   echo "  ->           Skipping Tests: ${SKIP_TESTS}"
+   echo "  ->               Is Dry-Run: ${DRY_RUN}"
 
-    # workaround for "No toolchain found with specification [version:1.8, vendor:default]" during release builds
-    cp -f .ci/maven_settings.xml $HOME/.m2/settings.xml
-    cp -f .ci/maven_toolchains.xml $HOME/.m2/toolchains.xml
+   # workaround for "No toolchain found with specification [version:1.8, vendor:default]" during release builds
+   cp -f .ci/maven_settings.xml $HOME/.m2/settings.xml
+   cp -f .ci/maven_toolchains.xml $HOME/.m2/toolchains.xml
 
-    # workaround for "Git fatal: ref HEAD is not a symbolic ref" during release
-    git checkout ${TRAVIS_BRANCH}
+   # workaround for "Git fatal: ref HEAD is not a symbolic ref" during release on Travis
+   git checkout ${GIT_BRANCH}
 
-    mvn -e -U --batch-mode --show-version \
-        -s .ci/maven_settings.xml -t .ci/maven_toolchains.xml \
-        -DskipTests=${SKIP_TESTS} -DskipITs=${SKIP_TESTS} \
-        -DdryRun=${DRY_RUN} -Dresume=false "-Darguments=-DskipTests=${SKIP_TESTS} -DskipITs=${SKIP_TESTS}" -DreleaseVersion=${POM_RELEASE_VERSION} -DdevelopmentVersion=${nextDevelopmentVersion} \
-        help:active-profiles clean release:clean release:prepare release:perform \
-        | grep -v -e "\[INFO\]  .* \[0.0[0-9][0-9]s\]" # the grep command suppresses all lines from maven-buildtime-extension that report plugins with execution time <=99ms
+   mvn $MAVEN_CLI_OPTS \
+      -DskipTests=${SKIP_TESTS} \
+      -DskipITs=${SKIP_TESTS} \
+      -DdryRun=${DRY_RUN} \
+      -Dresume=false \
+      "-Darguments=-DskipTests=${SKIP_TESTS} -DskipITs=${SKIP_TESTS}" \
+      -DreleaseVersion=${POM_RELEASE_VERSION} \
+      -DdevelopmentVersion=${nextDevelopmentVersion} \
+      help:active-profiles clean release:clean release:prepare release:perform \
+      | grep -v -e "\[INFO\]  .* \[0.0[0-9][0-9]s\]" # the grep command suppresses all lines from maven-buildtime-extension that report plugins with execution time <=99ms
 else
-    echo "###################################################"
-    echo "# Building Maven Project...                       #"
-    echo "###################################################"
-    if [[ ${TRAVIS_BRANCH} == "master" ]]; then
-        mavenGoal="deploy"
-    else
-        mavenGoal="verify"
-    fi
-    mvn -e -U --batch-mode --show-version \
-        -s .ci/maven_settings.xml -t .ci/maven_toolchains.xml \
-        help:active-profiles clean $mavenGoal \
-        | grep -v -e "\[INFO\]  .* \[0.0[0-9][0-9]s\]" # the grep command suppresses all lines from maven-buildtime-extension that report plugins with execution time <=99ms
+   echo
+   echo "###################################################"
+   echo "# Building Maven Project...                       #"
+   echo "###################################################"
+   if [[ ${GIT_BRANCH} == "master" && ${MAY_CREATE_RELEASE:-false} == "true" ]]; then
+      mavenGoal="deploy"
+   else
+      mavenGoal="verify"
+   fi
+   mvn $MAVEN_CLI_OPTS \
+      help:active-profiles clean $mavenGoal \
+      | grep -v -e "\[INFO\]  .* \[0.0[0-9][0-9]s\]" # the grep command suppresses all lines from maven-buildtime-extension that report plugins with execution time <=99ms
 fi
