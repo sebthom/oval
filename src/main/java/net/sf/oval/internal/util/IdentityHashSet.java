8b4ab3154c73cbcf45a42849864345382bb68fc1
sebthom <sebthom@users.noreply.github.com>
2020-12-30 14:53:29 +0100

    refactor IdentityHashSet

--- src/main/java/net/sf/oval/internal/util/IdentityHashSet.java
+++ src/main/java/net/sf/oval/internal/util/IdentityHashSet.java
@@ -9,15 +9,16 @@
  *********************************************************************/
 package net.sf.oval.internal.util;
 
-import static net.sf.oval.Validator.*;
-
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
+import java.io.IOException;
 import java.io.Serializable;
 import java.util.Collection;
+import java.util.IdentityHashMap;
 import java.util.Iterator;
-import java.util.Map;
 import java.util.Set;
+import java.util.Spliterator;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+import java.util.stream.Stream;
 
 /**
  * @author Sebastian Thomschke
@@ -25,38 +26,40 @@ import java.util.Set;
 public final class IdentityHashSet<E> implements Set<E>, Serializable {
    private static final long serialVersionUID = 1L;
 
-   private transient Map<Integer, E> map;
+   private final IdentityHashMap<E, Boolean> map;
+   private transient Set<E> entries;
 
    /**
     * Constructs a new, empty <tt>IdentitySet</tt>; the backing <tt>Map</tt> instance has
-    * default initial capacity (16) and load factor (0.75).
+    * default initial capacity (16).
     */
    public IdentityHashSet() {
-      map = getCollectionFactory().createMap();
+      map = new IdentityHashMap<>(16);
+      entries = map.keySet();
    }
 
    /**
     * Constructs a new, empty <tt>IdentitySet</tt>; the backing <tt>Map</tt> instance has
-    * the given initial capacity and the default load factor (0.75).
+    * the given initial capacity.
     */
    public IdentityHashSet(final int initialCapacity) {
-      map = getCollectionFactory().createMap(initialCapacity);
+      map = new IdentityHashMap<>(initialCapacity);
+      entries = map.keySet();
    }
 
    @Override
    public boolean add(final E o) {
-      final int hash = System.identityHashCode(o);
-      return map.put(hash, o) == null;
+      return map.put(o, Boolean.TRUE) == null;
    }
 
    @Override
    public boolean addAll(final Collection<? extends E> c) {
-      int count = 0;
+      boolean modified = false;
       for (final E e : c)
          if (add(e)) {
-            count++;
+            modified = true;
          }
-      return count > 0;
+      return modified;
    }
 
    @Override
@@ -66,13 +69,27 @@ public final class IdentityHashSet<E> implements Set<E>, Serializable {
 
    @Override
    public boolean contains(final Object o) {
-      final int hash = System.identityHashCode(o);
-      return map.containsKey(hash);
+      return map.containsKey(o);
    }
 
    @Override
    public boolean containsAll(final Collection<?> c) {
-      throw new UnsupportedOperationException();
+      return entries.containsAll(c);
+   }
+
+   @Override
+   public boolean equals(final Object o) {
+      return o == this || entries.equals(o);
+   }
+
+   @Override
+   public void forEach(final Consumer<? super E> action) {
+      entries.forEach(action);
+   }
+
+   @Override
+   public int hashCode() {
+      return entries.hashCode();
    }
 
    @Override
@@ -82,48 +99,37 @@ public final class IdentityHashSet<E> implements Set<E>, Serializable {
 
    @Override
    public Iterator<E> iterator() {
-      return map.values().iterator();
+      return entries.iterator();
    }
 
-   /**
-    * Reads the <tt>IdentitySet</tt> instance from a stream.
-    */
-   @SuppressWarnings("unchecked")
-   private void readObject(final ObjectInputStream ois) throws java.io.IOException, ClassNotFoundException {
-      // materialize any hidden serialization magic
-      ois.defaultReadObject();
+   @Override
+   public Stream<E> parallelStream() {
+      return entries.parallelStream();
+   }
 
-      // materialize the size
-      final int size = ois.readInt();
-
-      // materialize the elements
-      map = getCollectionFactory().createMap(size);
-      for (int i = 0; i < size; i++) {
-         final E o = (E) ois.readObject();
-         final int hash = System.identityHashCode(o);
-         map.put(hash, o);
-      }
+   private void readObject(final java.io.ObjectInputStream stream) throws IOException, ClassNotFoundException {
+      stream.defaultReadObject();
+      entries = map.keySet();
    }
 
    @Override
    public boolean remove(final Object o) {
-      final int hash = System.identityHashCode(o);
-      return map.remove(hash) != null;
+      return map.remove(o) != null;
    }
 
    @Override
    public boolean removeAll(final Collection<?> c) {
-      boolean modified = false;
-      for (final Object e : c)
-         if (remove(e)) {
-            modified = true;
-         }
-      return modified;
+      return entries.removeAll(c);
+   }
+
+   @Override
+   public boolean removeIf(final Predicate<? super E> filter) {
+      return entries.removeIf(filter);
    }
 
    @Override
    public boolean retainAll(final Collection<?> c) {
-      throw new UnsupportedOperationException();
+      return entries.retainAll(c);
    }
 
    @Override
@@ -132,28 +138,27 @@ public final class IdentityHashSet<E> implements Set<E>, Serializable {
    }
 
    @Override
+   public Spliterator<E> spliterator() {
+      return entries.spliterator();
+   }
+
+   @Override
+   public Stream<E> stream() {
+      return entries.stream();
+   }
+
+   @Override
    public Object[] toArray() {
-      return map.values().toArray();
+      return entries.toArray();
    }
 
    @Override
    public <T> T[] toArray(final T[] a) {
-      return map.values().toArray(a);
+      return map.keySet().toArray(a);
    }
 
-   /**
-    * Writes state of this <tt>IdentitySet</tt> instance to a stream.
-    */
-   private void writeObject(final ObjectOutputStream oos) throws java.io.IOException {
-      // serialize any hidden serialization magic
-      oos.defaultWriteObject();
-
-      // serialize the set's size
-      oos.writeInt(map.size());
-
-      // serialize the set's elements
-      for (final E e : map.values()) {
-         oos.writeObject(e);
-      }
+   @Override
+   public String toString() {
+      return entries.toString();
    }
 }
